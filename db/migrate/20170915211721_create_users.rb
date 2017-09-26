b48c3e599704226bc7a7074c43dc13fea28348c3
kylewstewart <kyle.w.stewart@mac.com>
2017-09-26 07:09:52 -0400

    update mobile schema to string

--- db/migrate/20170915211721_create_users.rb
+++ db/migrate/20170915211721_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.integer :mobile
+      t.string :mobile
       t.string  :otp_secret_key
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20170915211721) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.integer "mobile"
+    t.string "mobile"
     t.string "otp_secret_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
