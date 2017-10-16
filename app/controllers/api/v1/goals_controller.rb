58998c321c977985323eacf8a760ad5248ca504f
kylewstewart <kyle.w.stewart@mac.com>
2017-10-16 16:15:10 -0400

    Update Goal

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -11,7 +11,6 @@ class Api::V1::GoalsController < ApplicationController
 
   def create
     goal = Goal.create(goal_params)
-    render json: goal
   end
 
   def show
@@ -19,11 +18,12 @@ class Api::V1::GoalsController < ApplicationController
   end
 
   def edit
-    # form to edit an existing row
   end
 
   def update
-    #updates an existing row
+    goal = Goal.find(params[:id])
+    goal.update(goal_params)
+    render json: goal
   end
 
   def destroy
